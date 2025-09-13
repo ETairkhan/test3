2682d322a4ce9abb6e7c77dce0a4583eba721f0a
ETairkhan <trouzerop@gmail.com>
2025-09-13 13:00:47 +0500

    rebuilding the project with the wails

--- backend/internal/app/core/errors.go
+++
@@ -1,9 +0,0 @@
-package core
-
-import "errors"
-
-var (
-	ErrDBConn  = errors.New("db connection failure")
-	ErrTaskNotFound = errors.New("task is not exist")
-	ErrEmptyTaskText = errors.New("task is empty")
-)
